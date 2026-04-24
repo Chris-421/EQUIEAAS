1ade2244bbcb3d4185f0984136350209ac79b634
Chris-421 <177642947+Chris-421@users.noreply.github.com>
2026-04-24 13:39:33 +0200

    Fix Vercel font loading warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,14 +35,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
-        />
-      </head>
       <body
         className={cn(inter.variable, notoSerif.variable, plexMono.variable, 'font-sans')}
       >
